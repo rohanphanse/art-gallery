d4b8d530a4b065a1daa822a569e517d73bea0ce6
Rohan Phanse <58747665+rohanphanse@users.noreply.github.com>
2022-05-30 22:40:00 +0000

    Remove extraneous stuff

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { initializeContract } from "./utils/near";
 
 window.nearInitPromise = initializeContract()
@@ -14,5 +13,3 @@ window.nearInitPromise = initializeContract()
         );
     })
     .catch(console.error);
-
-reportWebVitals();
